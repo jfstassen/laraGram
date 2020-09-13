cde369d1bb9101a212eb5c61aacc98fff320fcab
jfstassen <jf.stassen@gmail.com>
2020-09-13 17:04:13 +0200

    2.19

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,24 +8,29 @@
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{$user->username}}</h1>
-                <a href="#">Add a new Post</a>
+                <h1>{{ $user->username }}</h1>
+                <a href="/p/create">Add a new Post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>123</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>18</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
-            <div>{{$user->profile->description}}</div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
             <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
     <div class="row pt-5">
-        <div class="col-4"><img class="w-100" src="https://picsum.photos/200/200" alt=""></div>
-        <div class="col-4"><img class="w-100" src="https://picsum.photos/200/200" alt=""></div>
-        <div class="col-4"><img class="w-100" src="https://picsum.photos/200/200" alt=""></div>
+        @foreach ($user->posts as $post)
+        <div class="col-4 pb-4">
+            <img class="w-100" src="/storage/{{ $post->image }}" alt="">
+        </div>
+        @endforeach
+
+
+
     </div>
 </div>
 @endsection
