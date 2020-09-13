dab9645dafdddffb2167853fd8e948acd6d1dd9e
jfstassen <jf.stassen@gmail.com>
2020-09-13 14:04:41 +0200

    1.30

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,16 +8,16 @@
         </div>
         <div class="col-9 pt-5">
             <div>
-                <h1>laraGram</h1>
+                <h1>{{$user->username}}</h1>
             </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>123</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>18</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">laragram.com</div>
-            <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, molestias.</div>
-            <div><a href="#">laragram.com</a></div>
+            <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
+            <div>{{$user->profile->description}}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
