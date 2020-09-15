959512f7cd4bd4326d835ef9a79e63481a32f4ea
jfstassen <jf.stassen@gmail.com>
2020-09-15 22:18:22 +0200

    3.22

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,7 +8,12 @@
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{ $user->username }}</h1>
+                <div class="d-flex align-items-center pb-3">
+                    <h1>{{ $user->username }}</h1>
+
+                    <button class="btn btn-primary ml-4">Follow</button>
+                </div>
+
                 @can('update', $user->profile)
                 <a href="/p/create">Add a new Post</a>
                 @endcan
