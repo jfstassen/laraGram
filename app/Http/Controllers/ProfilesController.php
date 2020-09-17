bf3e313308d3f5c06efb7a05f8ce278ed570a093
jfstassen <jf.stassen@gmail.com>
2020-09-17 19:20:54 +0200

    done caching 4.11

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
@@ -14,7 +15,20 @@ class ProfilesController extends Controller
 
         $follows = (auth()->user() ? auth()->user()->following->contains($user->id) : false);
         // dd($follows);
-        return view('profiles.index', compact("user", "follows"));
+
+        //refactor with cache of 30sec
+        ///////////////////////////// make up a cache key, giving us a unique key for each user
+        $postCount = Cache::remember("count.posts." . $user->id, now()->addSeconds(30), function () use ($user) {
+            return $user->posts->count();
+        });
+        $followersCount = Cache::remember("count.followers." . $user->id, now()->addSeconds(30), function () use ($user) {
+            return $user->profile->followers->count();
+        });
+        $followingCount = Cache::remember("count.following." . $user->id, now()->addSeconds(30), function () use ($user) {
+            return $user->following->count();
+        });
+
+        return view('profiles.index', compact("user", "follows", "postCount", "followersCount", "followingCount"));
     }
 
     public function edit(User $user)
@@ -42,10 +56,10 @@ class ProfilesController extends Controller
             $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
             $image->save();
 
-            $imageArray = ["image"=>$imagePath];
+            $imageArray = ["image" => $imagePath];
         }
         auth()->user()->profile->update(array_merge(
-            $data, 
+            $data,
             $imageArray ?? []
         ));
 
